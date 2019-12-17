fcb69da611489cf57d324b961000842ac1bc7a06
CRASHFIRE-666 <54841369+CRASHFIRE-666@users.noreply.github.com>
2019-12-17 15:15:35 +0300

    Create 13.1.cpp

---
+++ 13.1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+    cout << "Введите N" << endl;
+    int n;
+    cin >> n;
+    
+    vector<int> arr(n);
+    
+    for (int i = 0; i < n; i++)
+    {
+        arr[i] = i * 2 + 1;
+        cout << arr[i] << endl;
+    }
+    return 0;
+}
